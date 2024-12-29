a6a23ac38c6d2d4723e95ff9a290465e1c04f6d6
Kosi Anyanwu Jaden <Infinitrix900@gmail.com>
2024-12-29 14:21:46 +0100

    making a module

---
+++ datatimemodule.py
@@ -0,0 +1,9 @@
+# date time module 
+import datetime
+# the below code is used to get the current date time
+x = datetime.datetime.now()
+print(x)
+
+# write a code to find all the function name and vaariable names of the module
+y = dir(datetime)
+print(y)
---
+++ mathmodule.py
@@ -0,0 +1,11 @@
+import math
+# write a code to get the square root of hundread
+x = math.sqrt(90)
+print(x)
+
+# find all function names and variable of the above function
+x = dir(math)
+print(x)
+
+# get the function name and variable of the upper module
+print(len(x))
